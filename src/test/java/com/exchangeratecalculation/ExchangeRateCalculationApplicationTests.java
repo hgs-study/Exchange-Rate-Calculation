123db3754ec046544105ea791bb9c8c3639decdc
hgs-study <hgstudy_@naver.com>
2021-09-20 16:38:33 +0900

    Initial commit

---
+++ src/test/java/com/exchangeratecalculation/ExchangeRateCalculationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exchangeratecalculation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeRateCalculationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
